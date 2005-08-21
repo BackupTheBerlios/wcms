a5d388c36ac3ac0245717e8d3b1b84789cb30c1b
streaky <streaky>
2005-08-21 18:00:27 +0000

    Finished adding in license info.

--- wcms/boot.php
+++ wcms/boot.php
@@ -1,5 +1,37 @@
 <?php
 
+/**
+ * Project:     wCMS: Wiki style CMS
+ * File:        $Source: /home/xubuntu/berlios_backup/github/tmp-cvs/wcms/Repository/wcms/boot.php,v $
+ * Revision:    $Revision: 1.13 $
+ * Last Edit:   $Date: 2005/08/21 18:00:27 $
+ * By:          $Author: streaky $
+ *
+ *  Copyright © 2005 Martin Nicholls
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @link http://wcms.berlios.de/
+ * @copyright 2005 Martin Nicholls
+ * @author Martin Nicholls <webmasta at streakyland dot co dot uk>
+ * @package wCMS
+ * @version $Revision: 1.13 $
+ */
+
+/* $Id: boot.php,v 1.13 2005/08/21 18:00:27 streaky Exp $ */
+
 header("Cache-Control: must-revalidate");
 header("Expires: ".gmdate("D, d M Y H:i:s", time() + (60 * 60 * 24 * 3))." GMT");
 
--- wcms/index.php
+++ wcms/index.php
@@ -1,10 +1,40 @@
 <?php
 
+/**
+ * Project:     wCMS: Wiki style CMS
+ * File:        $Source: /home/xubuntu/berlios_backup/github/tmp-cvs/wcms/Repository/wcms/index.php,v $
+ * Revision:    $Revision: 1.3 $
+ * Last Edit:   $Date: 2005/08/21 18:00:27 $
+ * By:          $Author: streaky $
+ *
+ *  Copyright © 2005 Martin Nicholls
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @link http://wcms.berlios.de/
+ * @copyright 2005 Martin Nicholls
+ * @author Martin Nicholls <webmasta at streakyland dot co dot uk>
+ * @package wCMS
+ * @version $Revision: 1.3 $
+ */
+
+/* $Id: index.php,v 1.3 2005/08/21 18:00:27 streaky Exp $ */
+
 require_once("boot.php");
-//include(HEADER);
 
 $query = explode("/", $_SERVER['PATH_INFO']);
-//Var_Dump::display($query);
 
 switch ($query[1]) {
   case "news":
@@ -14,9 +44,5 @@ switch ($query[1]) {
     require_once(path::file()."content.php");
   break;
 }
-
-//echo time();
-
-//include(FOOTER);
 
 ?>
