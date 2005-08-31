b6633354312c8b14be7c14a5bc0cbc570f1bab9d
streaky <streaky>
2005-08-31 10:32:32 +0000

    Simpler pattern based file deletion [using glob()]

--- wcms/classes/cache_handling_class.php
+++ wcms/classes/cache_handling_class.php
@@ -3,8 +3,8 @@
 /**
  * Project:     wCMS: Wiki style CMS
  * File:        $Source: /home/xubuntu/berlios_backup/github/tmp-cvs/wcms/Repository/wcms/classes/cache_handling_class.php,v $
- * Revision:    $Revision: 1.8 $
- * Last Edit:   $Date: 2005/08/28 19:39:09 $
+ * Revision:    $Revision: 1.9 $
+ * Last Edit:   $Date: 2005/08/31 10:32:32 $
  * By:          $Author: streaky $
  *
  *  Copyright © 2005 Martin Nicholls
@@ -27,38 +27,38 @@
  * @copyright 2005 Martin Nicholls
  * @author Martin Nicholls <webmasta at streakyland dot co dot uk>
  * @package wCMS
- * @version $Revision: 1.8 $
+ * @version $Revision: 1.9 $
  */
 
-/* $Id: cache_handling_class.php,v 1.8 2005/08/28 19:39:09 streaky Exp $ */
+/* $Id: cache_handling_class.php,v 1.9 2005/08/31 10:32:32 streaky Exp $ */
 
 class cache_handler {
-	
+
 	var $_options = array(
-		'data_dir'        => 'cache/',
-		'cache_tag'       => '',
-		'file_perms'      => 0777,
-		'file_locking'    => true,
-		'lock_attempts'   => 500,
+	'data_dir'        => 'cache/',
+	'cache_tag'       => '',
+	'file_perms'      => 0777,
+	'file_locking'    => true,
+	'lock_attempts'   => 500,
 	);
-	
+
 	function cache_handler($options = array()) {
 		foreach ($options as $option => $value){
 			$this->_options[$option] = $value;
 		}
 	}
-	
+
 	function set($tag, $data) {
 		$data = serialize($data);
 		$data = "<?php\n{$data}\n?>";
-		
+
 		$data_dir = $this->_options['data_dir'];
 		$cache_tag = $this->_options['cache_tag'];
 		$cache_file_name = "{$data_dir}{$tag}_{$cache_tag}.cache.php";
-		
+
 		return $this->_write_file($cache_file_name, $data);
 	}
-	
+
 	function get($tag, $timeout = -1) {
 		$data_dir = $this->_options['data_dir'];
 		$cache_tag = $this->_options['cache_tag'];
@@ -76,70 +76,54 @@ class cache_handler {
 			return false;
 		}
 	}
-	
+
 	function clear($pattern) {
 		$this->_delete_pattern("{$pattern}*.cache.php");
 	}
-	
+
 	function _write_file($file_name, $content) {
 		$attempt = 0;
-        while ($attempt < $this->_options['lock_attempts']) {
-            $fp = @fopen($file_name, "wb");
-            if ($fp) {
-                if ($this->_options['file_locking'] == true) {
-                	@flock($fp, LOCK_EX);
-                }
-                $file_size = strlen($content);
-                @fwrite($fp, $content, $file_size);
-                @chmod($file_name, $this->_options['file_perms']);
+		while ($attempt < $this->_options['lock_attempts']) {
+			$fp = @fopen($file_name, "wb");
+			if ($fp) {
+				if ($this->_options['file_locking'] == true) {
+					@flock($fp, LOCK_EX);
+				}
+				$file_size = strlen($content);
+				@fwrite($fp, $content, $file_size);
+				@chmod($file_name, $this->_options['file_perms']);
 				@touch($file_name);
-                if ($this->_options['file_locking'] == true) {
-                	@flock($fp, LOCK_UN);
-                }
-                @fclose($fp);
-                return true;
-            }
-            $attempt ++;
-        }
+				if ($this->_options['file_locking'] == true) {
+					@flock($fp, LOCK_UN);
+				}
+				@fclose($fp);
+				return true;
+			}
+			$attempt ++;
+		}
 		return false;
 	}
-	
+
 	function _read_file($file_name) {
-        $fp = @fopen($file_name, "rb");
-        if ($this->_options['file_locking'] == true) {
-        	@flock($fp, LOCK_SH);
-        }
-        if ($fp) {
-            $contents = @fread($fp, filesize($file_name));
-            if ($this->_options['file_locking'] == true) {
-            	@flock($fp, LOCK_UN);
-            }
-            @fclose($fp);
-            return $contents;
-        }
-        return false;
-	}
-	
-	function _delete_pattern($pattern = "*.cache.php") {
-		$dir = $this->_options['data_dir'];
-		$deleted = false;
-		$pattern = str_replace(array("\*", "\?"), array(".*", "."), preg_quote($pattern));
-		if (substr($dir, -1) != "/") {
-			$dir .= "/";
+		$fp = @fopen($file_name, "rb");
+		if ($this->_options['file_locking'] == true) {
+			@flock($fp, LOCK_SH);
 		}
-		if (is_dir($dir)) {
-			$d = opendir($dir);
-			while ($file = readdir($d)) {
-				if (is_file($dir.$file) && ereg("^{$pattern}$", $file)) {
-					if (unlink($dir.$file)) {
-						$deleted[] = $file;
-					}
-				}
+		if ($fp) {
+			$contents = @fread($fp, filesize($file_name));
+			if ($this->_options['file_locking'] == true) {
+				@flock($fp, LOCK_UN);
 			}
-			closedir($d);
-			return true;
-		} else {
-			return false;
+			@fclose($fp);
+			return $contents;
+		}
+		return false;
+	}
+
+	function _delete_pattern($str = "*.cache.php") {
+		$dir = $this->_options['data_dir'];
+		foreach(glob("{$dir}{$str}") as $fn) {
+			@unlink($fn);
 		}
 	}
 }
